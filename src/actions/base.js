c1bb30f359362632c8f85cbf83f58dea892c82be
Michael Rincones <michael.rincones@projekt202.com>
2018-01-08 16:50:07 -0600

    inital commit

---
+++ src/actions/base.js
@@ -0,0 +1,16 @@
+window.myApp = window.myApp || {};
+
+myApp.actions = {
+
+    INIT_APPLICATION: 'INIT_APPLICATION',
+    BACK: 'BACK',
+    CHANGE_PAGE: 'CHANGE_PAGE',
+    ROUTE_CHANGED: 'ROUTE_CHANGED',
+};
+
+myApp.pages = {
+    VIEW1: 'view1',
+    VIEW2: 'view2',
+    VIEW3: 'view3',
+    VIEW404: 'view404',
+};
